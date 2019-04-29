2322053d71b0d2753c4461a42094f7f146ece40c
Christian Kellner <christian@kellner.me>
2019-04-29 18:24:28 +0200

    app: support for gamemode portal

--- app/window.c
+++ app/window.c
@@ -22,6 +22,14 @@
 #include "gamemode_client.h"
 
 #include <gio/gio.h>
+#include <gio/gunixfdlist.h>
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+
 
 struct _GmtWindow
 {
@@ -35,8 +43,10 @@ struct _GmtWindow
 
   /* config */
   gboolean uselib;
+  gboolean portal;
+
   /*  */
-  int      pid;
+  int pid;
 
   /*  */
   GDBusProxy *gamemode;
@@ -54,6 +64,9 @@ static void     on_bus_ready (GObject      *source,
 static void     gmt_builtin_gamemode_switch (GmtWindow *self,
                                              gboolean   enable);
 
+static void     gmt_portal_gamemode_switch (GmtWindow *self,
+                                            gboolean   enable);
+
 static void     on_builtin_switch_ready (GObject      *source,
                                          GAsyncResult *res,
                                          gpointer      user_data);
@@ -70,6 +83,9 @@ static void     on_uselib_notify (GObject    *gobject,
                                   GParamSpec *pspec,
                                   gpointer    user_data);
 
+int gmt_setup_socket (GmtWindow          *self,
+                      struct sockaddr_un *sau,
+                      socklen_t          *sl);
 /* private stuff */
 
 static void
@@ -90,12 +106,19 @@ gmt_window_class_init (GmtWindowClass *klass)
 #define GAMEMODE_DBUS_IFACE "com.feralinteractive.GameMode"
 #define GAMEMODE_DBUS_PATH "/com/feralinteractive/GameMode"
 
+#define PORTAL_DBUS_NAME "org.freedesktop.portal.Desktop"
+#define PORTAL_DBUS_IFACE "org.freedesktop.portal.GameMode"
+#define PORTAL_DBUS_PATH "/org/freedesktop/portal/desktop"
+
 static void
 gmt_window_init (GmtWindow *self)
 {
   g_autoptr(GCredentials) creds = NULL;
   g_autofree char *pidstr = NULL;
   GDBusProxyFlags flags;
+  gboolean portal = TRUE;
+
+  self->portal = TRUE;
 
   gtk_widget_init_template (GTK_WIDGET (self));
 
@@ -107,9 +130,9 @@ gmt_window_init (GmtWindow *self)
   flags = G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START_AT_CONSTRUCTION;
   g_dbus_proxy_new_for_bus (G_BUS_TYPE_SESSION,
                             flags, NULL,
-                            GAMEMODE_DBUS_NAME,
-                            GAMEMODE_DBUS_PATH,
-                            GAMEMODE_DBUS_IFACE,
+                            portal ? PORTAL_DBUS_NAME : GAMEMODE_DBUS_NAME,
+                            portal ? PORTAL_DBUS_PATH : GAMEMODE_DBUS_PATH,
+                            portal ? PORTAL_DBUS_IFACE : GAMEMODE_DBUS_IFACE,
                             NULL,
                             on_bus_ready,
                             self);
@@ -127,6 +150,7 @@ on_bus_ready (GObject      *source,
 {
   g_autoptr(GError) err = NULL;
   GmtWindow *self;
+  const char *name;
 
   self = GMT_WINDOW (user_data);
 
@@ -137,6 +161,9 @@ on_bus_ready (GObject      *source,
       g_warning ("could not create gamemode proxy: %s", err->message);
       return;
     }
+  name = g_dbus_connection_get_unique_name (g_dbus_proxy_get_connection (self->gamemode));
+
+  g_debug ("my name: %s", name);
 
   gtk_widget_set_sensitive (GTK_WIDGET (self->sw_gamemode), TRUE);
 }
@@ -154,6 +181,8 @@ on_gamemode_toggled (GmtWindow *self,
 
   if (self->uselib)
     gmt_library_gamemode_switch (self, enable);
+  else if (self->portal)
+    gmt_portal_gamemode_switch (self, enable);
   else
     gmt_builtin_gamemode_switch (self, enable);
 
@@ -213,6 +242,188 @@ on_uselib_notify (GObject    *gobject,
 }
 
 /* native gamemode implementation */
+typedef struct PortalCall
+{
+  GmtWindow *wnd;
+  int        wire[2];
+  int        data[2];
+} PortalCall;
+
+static gboolean
+make_socketpair (int domain, int type, int proto, int sv[2], GError **error)
+{
+  int r;
+
+  r = socketpair (domain, type, proto, sv);
+
+  if (r == -1)
+    {
+      g_set_error (error, G_IO_ERROR, g_io_error_from_errno (errno),
+                   "could not create socket pair: %s",
+                   g_strerror (errno));
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+static void
+portal_call_destroy (PortalCall *call)
+{
+  if (call == NULL)
+    return;
+
+  close (call->wire[0]);
+  close (call->wire[1]);
+
+  close (call->data[0]);
+  close (call->data[1]);
+
+  g_object_unref (call->wnd);
+  g_slice_free (PortalCall, call);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (PortalCall, portal_call_destroy);
+
+PortalCall *
+gmt_window_portal_call_new (GmtWindow *self, GError **error)
+{
+  g_autoptr(PortalCall) call = NULL;
+  gboolean ok;
+
+  call = g_slice_new0 (PortalCall);
+
+  ok = make_socketpair (AF_UNIX, SOCK_STREAM, 0, call->wire, error);
+
+  if (!ok)
+    return NULL;
+
+  ok = make_socketpair (AF_UNIX, SOCK_STREAM, 0, call->data, error);
+
+  if (!ok)
+    return NULL;
+
+  call->wnd = g_object_ref (self);
+
+  return g_steal_pointer (&call);
+}
+
+static int
+send_fd (int sock, const char *data, size_t len, int fd)
+{
+  struct iovec iov = {
+    .iov_base = (void *) data,
+    .iov_len = len,
+  };
+
+  union
+  {
+    struct cmsghdr cmh;
+    char           data[CMSG_SPACE (sizeof (int))];
+  } ctrl = {};
+  struct msghdr msg = {
+    .msg_iov = &iov,
+    .msg_iovlen = 1,
+  };
+  int r;
+
+  if (fd > 0)
+    {
+      struct cmsghdr *cmh;
+
+      msg.msg_control = &ctrl;
+      msg.msg_controllen = sizeof (ctrl);
+
+      cmh = CMSG_FIRSTHDR (&msg);
+
+      cmh->cmsg_level = SOL_SOCKET;
+      cmh->cmsg_type = SCM_RIGHTS;
+      cmh->cmsg_len = CMSG_LEN (sizeof (int));
+
+      memcpy (CMSG_DATA (cmh), &fd, sizeof (int));
+
+    }
+
+  r = sendmsg (sock, &msg, MSG_NOSIGNAL);
+
+  if (r < 0)
+    return -errno;
+
+  return 0;
+}
+
+
+static void
+on_portal_switch_ready (GObject      *source,
+                        GAsyncResult *res,
+                        gpointer      user_data)
+{
+  g_autoptr(GError) err = NULL;
+  g_autoptr(GVariant) val = NULL;
+  g_autoptr(PortalCall) call = user_data;
+  GmtWindow *self = call->wnd;
+  int r = -1;
+
+  val =  g_dbus_proxy_call_with_unix_fd_list_finish (G_DBUS_PROXY (self->gamemode),
+                                                     NULL,
+                                                     res,
+                                                     &err);
+  if (val == NULL)
+    {
+      g_warning ("could not talk to gamemode: %s", err->message);
+    }
+  else
+    {
+      g_variant_get (val, "(i)", &r);
+
+      if (r != 0)
+        g_warning ("request got rejected: %d", r);
+    }
+
+  gamemode_toggle_finish (self, r);
+}
+
+
+static void
+gmt_portal_gamemode_switch (GmtWindow *self,
+                            gboolean   enable)
+{
+  g_autoptr(GError) err = NULL;
+  g_autoptr(GUnixFDList) fds = NULL;
+  g_autofree char *path = NULL;
+  const char *mode = enable ? "RegisterGame" : "UnregisterGame";
+  PortalCall *call;
+  ssize_t r;
+
+  call = gmt_window_portal_call_new (self, &err);
+
+  if (call == NULL)
+    {
+      g_warning ("could not create call: %s", err->message);
+      gamemode_toggle_finish (self, -1);
+      return;
+    }
+
+  fds = g_unix_fd_list_new_from_array (&call->wire[1], 1);
+
+  g_dbus_proxy_call_with_unix_fd_list (G_DBUS_PROXY (self->gamemode),
+                                       "Action",
+                                       g_variant_new ("(h)", 0),
+                                       G_DBUS_CALL_FLAGS_NONE,
+                                       -1,
+                                       fds,
+                                       NULL, /* cancel */
+                                       on_portal_switch_ready,
+                                       call);
+
+  g_debug ("sending fd");
+  r = send_fd (call->wire[0], mode, strlen (mode), call->data[1]);
+  if (r < 0)
+    g_warning ("could not send fd: %s", g_strerror (errno));
+  else
+    g_debug ("fd sent");
+}
+
 
 static void
 gmt_builtin_gamemode_switch (GmtWindow *self,
@@ -234,7 +445,6 @@ gmt_builtin_gamemode_switch (GmtWindow *self,
                      on_builtin_switch_ready,
                      self);
 }
-
 
 static void
 on_builtin_switch_ready (GObject      *source,
